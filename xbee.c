dde328fe368e0c8080aba04d1fe53af5e0bc3561
Konke <koradlow@gmail.com>
2012-10-25 20:00:17 +0000

    fixed setup of serial port

--- xbee.c
+++ xbee.c
@@ -26,6 +26,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <time.h>
 #include <termios.h>	/* POSIX terminal control definitions */
 
 #define SERIAL_PORT "/dev/ttyAMA0"
@@ -35,7 +36,7 @@
  * @ret_val:	0 on success, -1 on error */
 int open_serial(const char* device) {
 	int fd = -1; 
-	fd = open(device, O_RDWR);
+	fd = open(device, O_RDWR | O_NOCTTY);
 	if (fd < 0)
 		fprintf(stderr, "Unable to open %s", device);
 	return fd;
@@ -45,24 +46,41 @@ int open_serial(const char* device) {
  * @fd:		file descriptor for a terminal device
  * @ret_val:	0 on success, -1 on error */
 int setup_serial(int fd) {
-	struct termios new_settings, old_settings;
+	struct termios settings, old_settings;
 
 	/* Get the current settings for the port */
 	if (tcgetattr(fd, &old_settings)) {
 		perror("tcgetattr: ");
 		return -1;
 	}
-	new_settings = old_settings;
-
-	/* set to B9600-8N1, allow input to be received */
-	new_settings.c_cflag = B9600 | CS8 | CREAD;
-	/* disable hardware flow control */
-	new_settings.c_cflag &= ~CRTSCTS; 
+	settings = old_settings;
 	
+	// setup baudrate
+	settings.c_cflag &= ~CBAUD;
+	settings.c_cflag |= B9600;
+
+	// Ignore modem status lines & allow input to be received
+	settings.c_cflag |= (CLOCAL | CREAD);
+	
+	// disable parity
+	settings.c_cflag &= ~PARENB;
+
+	// one stop bit
+	settings.c_cflag &= ~CSTOPB;
+
+	// data size set to 8bits
+	settings.c_cflag &= ~CSIZE;
+	settings.c_cflag |= CS8;
+
+	// disable flow control
+	settings.c_cflag &= ~CRTSCTS;
+
+	settings.c_cc[VMIN] = 0;
+	settings.c_cc[VTIME] = 1; // 0.1s
+
 	/* Set the new settings */
 	/* TSCNOW -> change occurs immediately */
-	tcflush(fd, TCIFLUSH);
-	if (tcsetattr(fd, TCSANOW, &new_settings) < 0) {
+	if (tcsetattr(fd, TCSANOW, &settings) < 0) {
 		perror("tcsetattr: ");
 		return -1;
 	}
@@ -83,11 +101,33 @@ int main(int argc, char **argv) {
 		exit(-1);
 
 	/* set the xbee into command mode */
-	strcpy("+++", &buf[0]);
+	strcpy(&buf[0], "+++");
 	write(fd, &buf, 3);
 	printf("setting xbee into command mode\n"); 
+	//sleep(1);
 	byte_cnt = read(fd, &buf, 2);
+	buf[byte_cnt] = '\0';
 	printf("read %d bytes from serial: %s\n", byte_cnt, buf);
 
+	/* try to read the PAN ID */
+	strcpy(&buf[0], "ATBD");
+	//buf[4] = 0x0d; //CR
+	write(fd, &buf, 4);
+	//sleep();
+	byte_cnt = read(fd, &buf, 1);
+	buf[byte_cnt] = '\0';
+	printf("ATBD: %s, byte cnt=%d\n", buf, byte_cnt);
+	
+	/* try to read the PAN ID */
+	//strcpy(&buf[0], "ATID");
+	//buf[4] = 0x0d; //CR
+	//write(fd, &buf, 4);
+	//sleep(2);
+	//byte_cnt = read(fd, &buf, 4);
+	//buf[byte_cnt] = '\0';
+	//printf("PAN Id: %s, byte cnt=%d\n", buf, byte_cnt);
+
+	/* close the serial port and exit the program */
+	close(fd);
 	return 1;
 }
